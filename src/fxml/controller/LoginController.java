b9eb02bef118f0ff971e335177ef649a10d7ebb8
Afonso Alcantara <alcantara.afonso@gmail.com>
2023-01-07 11:43:11 -0300

    ControlsFX lib

--- src/fxml/controller/LoginController.java
+++ src/fxml/controller/LoginController.java
@@ -1,6 +1,9 @@
 package fxml.controller;
 
+import org.controlsfx.control.Notifications;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
 
 public class LoginController {
@@ -10,9 +13,27 @@ public class LoginController {
 	private TextField senhaField;
 	
 	public void entrar() {
-		System.out.println("Entrar...");
-		System.out.println("Entrar..." + emailField.getText());
-		System.out.println("Entrar..." + senhaField.getText());
+		
+		boolean emailValido = emailField.getText().equals("afonso@beganinha.ccom.br");
+		boolean senhaValido = senhaField.getText().equals("12345");
+		
+		if (emailValido && senhaValido) {
+			Notifications
+				.create()
+				.position(Pos.TOP_RIGHT)
+				.title("Login FXML")
+				.text("Login efetuado com sucesso")
+				.showInformation();
+			
+		} else {
+			Notifications
+			.create()
+			.position(Pos.TOP_RIGHT)
+			.title("Login FXML")
+			.text("Usuário e Senha inválidos")
+			.showError();
+			
+		}
 		
 	}
 	
